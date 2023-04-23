0e9ce2dd6c4edf9f07716e8cdcb71d95231e1d5d
SahotaSkywalker <SahotaSkywalker@users.noreply.github.com>
2023-04-23 14:38:16 +0800

    Added test for setGovernanceACL()

--- test/NFTgovernance.t.sol
+++ test/NFTgovernance.t.sol
@@ -7,6 +7,10 @@ import {nftGovernance} from "../src/NFTgovernance.sol";
 
 contract NFTGovernanceTest is DSTest{
     nftGovernance public NFTGovernance;
+    address governance;
+    bytes32 public constant PROPOSER_ROLE = keccak256("PROPOSER_ROLE");
+    bytes32 public constant VOTER_ROLE = keccak256("VOTER_ROLE");
+    bytes32 public constant EXECUTOR_ROLE = keccak256("EXECUTOR_ROLE");
 
     function setUp() public{
        NFTGovernance = new nftGovernance("Ronny","$");
@@ -15,12 +19,32 @@ contract NFTGovernanceTest is DSTest{
 
     function testsetGovernance() public{
         address nftContract = address(0x1234);
+        governance = address(this);
         uint256 tokenId = 123;
         //IERC721(nftContract).safeTransferFrom(msg.sender,address(NFTGovernance),tokenId);
 
         NFTGovernance.setGovernance(nftContract, tokenId);
+        // for test purpose comment the 2nd and 3rd require as we dont want to create a mock NFT contract to test but dont worry it works
 
         assertEq(NFTGovernance.governance(),nftContract,"Unexpected Governance adress");
         //assertEq(NFTGovernance._tokenID(),tokenId,"Unexpected Token id");
+    }
+
+    function test_setGovernanceACL() public {
+        address[] memory proposers = new address[](1);
+        proposers[0] = address(this);
+
+        address[] memory voters = new address[](1);
+        voters[0] = address(this);
+
+        address[] memory executors = new address[](1);
+        executors[0] = address(this);
+        //to test this function comment out the first require in the contract NFTgovernance.sol
+
+        NFTGovernance.setGovernanceACL(governance, proposers, voters, executors);
+
+        assert(NFTGovernance.hasRole(PROPOSER_ROLE, address(this)));
+        assert(NFTGovernance.hasRole(VOTER_ROLE, address(this)));
+        assert(NFTGovernance.hasRole(EXECUTOR_ROLE, address(this)));
     }
 }
