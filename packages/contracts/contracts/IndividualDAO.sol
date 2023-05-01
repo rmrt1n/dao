656fc1125468484e2c16a2d77515154054f28fe6
Yudhish Maran <66850474+0xYudhishthra@users.noreply.github.com>
2023-05-02 05:07:44 +0800

    feat: complete initial version of individualDAO creator

--- packages/contracts/contracts/IndividualDAO.sol
+++ packages/contracts/contracts/IndividualDAO.sol
@@ -5,6 +5,9 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 import "@aragon/osx/framework/dao/DAOFactory.sol";
+import "@aragon/osx/framework/plugin/repo/PluginRepo.sol";
+import {PluginSetupRef} from "@aragon/osx/framework/plugin/setup/PluginSetupProcessorHelpers.sol";
+
 import "./AttestationStation.sol";
 
 // Only the owner can change the constructor parameters
@@ -14,42 +17,45 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 /// @notice Custom DAOFactory for creating individual DAOs with selected plugins.
 contract IndividualDAO is Ownable {
     DAOFactory public daoFactory;
+    PluginRepo public pluginRepo;
     AttestationStation public attestationStationContract;
     uint256 public minAttestations;
+    bytes32 public confirmDAOCreationKey;
 
-    /// @param _daoFactoryAddress The address of the DAOFactory contract deployed on the network.
+    error AddressDidNotAttest(address _address);
+
+    /// @param _daoFactory The address of the DAOFactory contract deployed on the network.
     constructor(
-        address _daoFactoryAddress,
+        address _daoFactory,
         address _attestationStationProxy,
-        uint256 _minAttestations
+        uint256 _minAttestations,
+        string memory _recoveryKey
     ) {
-        daoFactory = DAOFactory(_daoFactoryAddress);
+        daoFactory = DAOFactory(_daoFactory);
         attestationStationContract = AttestationStation(
             _attestationStationProxy
         );
         minAttestations = _minAttestations;
+        confirmDAOCreationKey = keccak256(bytes(_recoveryKey));
     }
 
     //the modifier accepts an array of addresses, the salt used to get the DAO address, and the recovery key
-    modifier _isDAOAttested(
-        address[] memory _recoveryAccounts,
-        bytes32 _salt,
-        bytes32 _recoveryKey
-    ) {
-        //get the DAO address from the salt
-        address daoAddress = daoFactory.computeAddress(_salt);
-
-        //use a for loop to check each address in the array, and only use require statement if any false is found
-        for (uint256 i = 0; i < _recoveryAccounts.length; i++) {
+    modifier _isDAOAttested(address[] memory _initialDAOMembers) {
+        for (uint256 i = 0; i < _initialDAOMembers.length; i++) {
             //check if the address is attested
-            require(
-                attestationStationContract.attestations(
-                    _recoveryAccounts[i],
-                    address(this),
-                    _recoveryKey == 
-                ),
-                "Address not attested"
+            uint confirm = 1;
+            bytes memory confirmBytes = abi.encodePacked(confirm);
+            bytes memory _val = attestationStationContract.attestations(
+                _initialDAOMembers[i],
+                msg.sender,
+                confirmDAOCreationKey
             );
+            if (
+                keccak256(abi.encodePacked(_val)) !=
+                keccak256(abi.encodePacked(confirmBytes))
+            ) {
+                revert AddressDidNotAttest(_initialDAOMembers[i]);
+            }
         }
         _;
     }
@@ -68,79 +74,72 @@ contract IndividualDAO is Ownable {
         daoFactory = DAOFactory(_daoFactoryAddress);
     }
 
-    /// @notice Creates a new DAO, registers it on the  DAO registry, and installs a list of plugins via the plugin setup processor.
-    /// @param _daoSettings The DAO settings to be set during the DAO initialization.
-    /// @param _pluginSettings The array containing references to plugins and their settings to be installed after the DAO has been created.
-    // function createDao(
-    //     DAOSettings calldata _daoSettings,
-    //     PluginSettings[] calldata _pluginSettings
-    // ) public override returns (DAO createdDao) {
-    //     // Check if no plugin is provided.
-    //     if (_pluginSettings.length == 0) {
-    //         revert NoPluginProvided();
-    //     }
-    //     // Create DAO.
-    //     createdDao = _createDAO(_daoSettings);
-    //     // Register DAO.
-    //     daoRegistry.register(createdDao, msg.sender, _daoSettings.subdomain);
-    //     // Get Permission IDs
-    //     bytes32 rootPermissionID = createdDao.ROOT_PERMISSION_ID();
-    //     bytes32 applyInstallationPermissionID = pluginSetupProcessor
-    //         .APPLY_INSTALLATION_PERMISSION_ID();
-    //     // Grant the temporary permissions.
-    //     // Grant Temporarly `ROOT_PERMISSION` to `pluginSetupProcessor`.
-    //     createdDao.grant(
-    //         address(createdDao),
-    //         address(pluginSetupProcessor),
-    //         rootPermissionID
-    //     );
-    //     // Grant Temporarly `APPLY_INSTALLATION_PERMISSION` on `pluginSetupProcessor` to this `DAOFactory`.
-    //     createdDao.grant(
-    //         address(pluginSetupProcessor),
-    //         address(this),
-    //         applyInstallationPermissionID
-    //     );
-    //     // Install plugins on the newly created DAO.
-    //     for (uint256 i; i < _pluginSettings.length; ++i) {
-    //         // Prepare plugin.
-    //         (
-    //             address plugin,
-    //             IPluginSetup.PreparedSetupData memory preparedSetupData
-    //         ) = pluginSetupProcessor.prepareInstallation(
-    //                 address(createdDao),
-    //                 PluginSetupProcessor.PrepareInstallationParams(
-    //                     _pluginSettings[i].pluginSetupRef,
-    //                     _pluginSettings[i].data
-    //                 )
-    //             );
-    //         // Apply plugin.
-    //         pluginSetupProcessor.applyInstallation(
-    //             address(createdDao),
-    //             PluginSetupProcessor.ApplyInstallationParams(
-    //                 _pluginSettings[i].pluginSetupRef,
-    //                 plugin,
-    //                 preparedSetupData.permissions,
-    //                 hashHelpers(preparedSetupData.helpers)
-    //             )
-    //         );
-    //     }
-    //     // Set the rest of DAO's permissions.
-    //     _setDAOPermissions(createdDao);
-    //     // Revoke the temporarly granted permissions.
-    //     // Revoke Temporarly `ROOT_PERMISSION` from `pluginSetupProcessor`.
-    //     createdDao.revoke(
-    //         address(createdDao),
-    //         address(pluginSetupProcessor),
-    //         rootPermissionID
-    //     );
-    //     // Revoke `APPLY_INSTALLATION_PERMISSION` on `pluginSetupProcessor` from this `DAOFactory` .
-    //     createdDao.revoke(
-    //         address(pluginSetupProcessor),
-    //         address(this),
-    //         applyInstallationPermissionID
-    //     );
-    //     // Revoke Temporarly `ROOT_PERMISSION_ID` from `pluginSetupProcessor` that implecitly granted to this `DaoFactory`
-    //     // at the create dao step `address(this)` being the initial owner of the new created DAO.
-    //     createdDao.revoke(address(createdDao), address(this), rootPermissionID);
-    // }
+    //we will first try to connect to the admin repo using the PluginRepoFactory
+    function getSetupRef(
+        address _pluginRepo
+    ) internal view returns (PluginSetupRef memory) {
+        //first connect to the intended plugin repo, e.g. token-voting-repo
+        //use proxy address when connecting to the repo
+        PluginRepo currentPluginRepo = PluginRepo(_pluginRepo);
+
+        //now get the latest release
+        uint8 latestRelease = currentPluginRepo.latestRelease();
+
+        //then get the latest version tag,return value is a tuple
+        PluginRepo.Version memory version = currentPluginRepo.getLatestVersion(
+            latestRelease
+        );
+
+        // this is points to the current version of the token voting app in the token voting repo
+        PluginSetupRef memory currentSetupRef = PluginSetupRef({
+            pluginSetupRepo: currentPluginRepo,
+            versionTag: version.tag
+        });
+
+        //return the setupRef
+        return currentSetupRef;
+    }
+
+    function getEncodedSetupData(
+        address _pluginSetupRepo
+    ) internal view returns (DAOFactory.PluginSettings memory) {
+        bytes memory encodedSetupData = abi.encode(
+            address(0x8dC75164dab325Dda81022cE7ee7a2B23a207C04)
+        );
+
+        //now get the encoded setup data
+        DAOFactory.PluginSettings memory pluginSettings = DAOFactory
+            .PluginSettings({
+                pluginSetupRef: getSetupRef(_pluginSetupRepo),
+                data: encodedSetupData
+            });
+
+        return pluginSettings;
+    }
+
+    function createDAO(
+        address[] memory _recoveryAccounts,
+        address _pluginRepo
+    ) external _isDAOAttested(_recoveryAccounts) {
+        DAOFactory.PluginSettings[]
+            memory pluginSettings = new DAOFactory.PluginSettings[](1);
+
+        //instantiate the DAOSettings struct
+        DAOFactory.DAOSettings memory daoSettings = DAOFactory.DAOSettings({
+            trustedForwarder: address(0),
+            daoURI: "",
+            subdomain: "",
+            metadata: ""
+        });
+
+        //insert values in PluginSettings array
+        DAOFactory.PluginSettings memory pluginSetting = getEncodedSetupData(
+            _pluginRepo
+        );
+
+        pluginSettings[0] = pluginSetting;
+
+        //create the DAO
+        daoFactory.createDao(daoSettings, pluginSettings);
+    }
 }
